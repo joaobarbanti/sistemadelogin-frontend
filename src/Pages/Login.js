5cb17c152814b2986c690912bedff0571119bfe8
joaoalado <joaobarbanti7@gmail.com>
2022-12-10 02:26:24 -0300

    arrumando rotas

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {  NavLink, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import api from "../services/api";
 
 
